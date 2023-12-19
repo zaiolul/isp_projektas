70955eaf9c755e409c1573ea01f0974fb5b7d408
Hexxy117 <vilius.malinauskas117@gmail.com>
2023-12-19 05:08:17 +0200

    stuff I dont remember anymore

---
+++ ISP_PROJEKTAS.Server/Models/Ingredientas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ISP_PROJEKTAS.Server.Models
+{
+	public class Ingredientas
+	{
+		[Column("pavadinimas")]
+		public string pavadinimas { get; set; }
+		[Key]
+		[Column("id_Ingredientas")]
+		public int Id_Ingredientas { get; set; }
+
+
+		[Column("fk_Patiekalasid_Patiekalas")]
+		public int Fk_Patiekalasid { get; set; }
+
+
+	}
+}
